a4adebfa30b6307b53481c455ce4daa209ff6170
Thang <thang27302@gmail.com>
2023-10-24 15:51:11 +0700

    fix form

--- src/app/(auth)/forgetPassword/ForgerForm.tsx
+++ src/app/(auth)/forgetPassword/ForgerForm.tsx
@@ -27,32 +27,30 @@ function ForgerForm() {
       <h2 className="text-center font-bold text-3xl text-[#473C33]">
         Quên mật khẩu
       </h2>
-      <form className="flex flex-col w-4/5 mx-auto mt-7">
-        <div className="flex flex-col gap-6 justify-center">
-          <input
-            type="text"
-            placeholder="Số điện thoại"
-            className="outline-none border-none p-3 rounded-2xl"
-            onChange={(e) => setPhone(e.target.value)}
-          />
+      <div className="flex flex-col gap-6 justify-center">
+        <input
+          type="text"
+          placeholder="Số điện thoại"
+          className="outline-none border-none p-3 rounded-2xl"
+          onChange={(e) => setPhone(e.target.value)}
+        />
 
-          <Otp setOtp={setOtp} phone={phone} />
-        </div>
+        <Otp setOtp={setOtp} phone={phone} />
+      </div>
 
-        <button
-          className="bg-[#FFB761] text-[#473C33] text-2xl rounded-2xl p-2 mt-4 font-bold"
-          onClick={verifyOtp}
-        >
-          Xác thực OTP
-        </button>
+      <button
+        className="bg-[#FFB761] text-[#473C33] text-2xl rounded-2xl p-2 mt-4 font-bold"
+        onClick={verifyOtp}
+      >
+        Xác thực OTP
+      </button>
 
-        <h4 className="text-right mt-1 text-[#473C33]">
-          Đã có tài khoản?{" "}
-          <Link href={"/signIn"} className="hover:underline">
-            Đăng nhập
-          </Link>
-        </h4>
-      </form>
+      <h4 className="text-right mt-1 text-[#473C33]">
+        Đã có tài khoản?{" "}
+        <Link href={"/signIn"} className="hover:underline">
+          Đăng nhập
+        </Link>
+      </h4>
 
       {/* <ForgerModal setOpen={setOpen} phone={phone} /> */}
       {open && <ForgerModal setOpen={setOpen} phone={phone} />}
